a13e750ca162972b31d025e07bbed170fb502fde
trp4145 <trp4145@bell.net>
2017-01-24 00:39:23 -0500

    Alpha 0.01
    *Added Player
    *Added basic control for Android
    *Added stars (Particle effects)
    *Added engine particle effects
    *Added lasers shooting from spacecraft

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = new Vector3 (0, 0, 10);
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x,Input.GetTouch(0).position.y))+offset;
+		//pos = gameObject.transform.position;
+
+	}
+}
